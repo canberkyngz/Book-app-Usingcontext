2616f1a39247e82b36d214591981ee6e619ea090
canberkyngz <cyangaz81@gmail.com>
2023-09-26 18:24:23 +0300

    books-usingContext

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
+import { useContext } from 'react';
+
+function BookList() {
+  const {books}=useContext(BooksContext)
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow key={book.id} book={book} />
+    );
+  });
+
+  return <div className="book-list">
+   
+    {renderedBooks}</div>;
+}
+
+export default BookList;
